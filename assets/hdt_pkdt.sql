280e05a176f887927d487b9ad8239a777e5b82bd
Hes <140692002+BaoduySbZ@users.noreply.github.com>
2024-12-19 13:40:22 +0700

    Update hdt_pkdt.sql

--- assets/hdt_pkdt.sql
+++ assets/hdt_pkdt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 18, 2024 lúc 07:04 PM
+-- Thời gian đã tạo: Th12 19, 2024 lúc 07:39 AM
 -- Phiên bản máy phục vụ: 10.4.27-MariaDB
 -- Phiên bản PHP: 7.4.33
 
